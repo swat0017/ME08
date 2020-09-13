fd455fa1b8783f62d2aeebbc6798e6a8868712ed
DarsiniKrishnakumar <70090040+DarsiniKrishnakumar@users.noreply.github.com>
2020-09-14 03:50:49 +1000

    Update APIGatewayConnection.java

--- app/src/main/java/com/example/meo8/networkconnection/APIGatewayConnection.java
+++ app/src/main/java/com/example/meo8/networkconnection/APIGatewayConnection.java
@@ -204,6 +204,22 @@ public class APIGatewayConnection {
         }
         return results;
     }
+    public String getAllOrganisationList() {
+        final String methodPath = "/organisationlist/allorganisations";
+        System.out.println(methodPath);
+        Request.Builder builder = new Request.Builder();
+        builder.url(BASE_URL + methodPath);
+        Request request = builder.build();
+        try {
+            Response response = client.newCall(request).execute();
+            results = response.body().string();
 
+            System.out.println(results);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return results;
+    
+    }
 
 }
