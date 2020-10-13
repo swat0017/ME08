ccdcb1ef32b2c848a2070ae28042265c1448cce7
swat0017 <49256142+swat0017@users.noreply.github.com>
2020-10-13 16:20:05 +1100

    Update WelcomeFragment.java
    card changes in welcome screen

--- app/src/main/java/com/example/meo8/WelcomeFragment.java
+++ app/src/main/java/com/example/meo8/WelcomeFragment.java
@@ -12,12 +12,25 @@ import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
+
+import com.example.me08.adapter.ViewCategoryAdapter;
+import com.example.me08.adapter.ViewPageAdapter;
+import com.tbuonomo.viewpagerdotsindicator.WormDotsIndicator;
 
 
-    public class WelcomeFragment extends Fragment {
+public class WelcomeFragment extends Fragment {
         Button analysis;
         Button organisation;
         Button maplayout;
+        CardView categoryCard;
+        CardView speciesCard;
+        CardView prepostcard;
+        CardView findCard;
+    private ViewCategoryAdapter viewPageAdapter;
+    private ViewPager viewPager;
+    WormDotsIndicator sp;
+    boolean val=false;
         public WelcomeFragment() {
         }
 
@@ -28,39 +41,50 @@ import androidx.fragment.app.Fragment;
 
 
             View view = inflater.inflate(R.layout.welcome_fragment, container, false);
-           analysis=(Button)view.findViewById(R.id.buttonanalysis);
-            maplayout=(Button)view.findViewById(R.id.buttonloc);
-            organisation=(Button)view.findViewById(R.id.buttonorg);
-           analysis.setOnClickListener((new View.OnClickListener() {
-               @Override
-               public void onClick(View view) {
-
-              getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                           new RiskAnalysisFragment()).addToBackStack(null).commit();
-
-
-               }}));
-
-
-            maplayout.setOnClickListener((new View.OnClickListener() {
+            sp=(WormDotsIndicator)view.findViewById(R.id.dotswelcome);
+            viewPager=(ViewPager)view.findViewById(R.id.viewpagewelcome);
+            viewPageAdapter=new ViewCategoryAdapter(getChildFragmentManager());
+            viewPager.setAdapter(viewPageAdapter);
+            sp.setViewPager(viewPager);
+          /*  categoryCard=(CardView)view.findViewById(R.id.categoryrisk);
+          categoryCard.setOnClickListener(new View.OnClickListener() {
+              @Override
+              public void onClick(View view) {
+                  getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                          new InformationFragment()).addToBackStack(null).commit();
+          }
+        });*/
+            speciesCard=(CardView)view.findViewById(R.id.speciesrisk);
+            speciesCard.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
+                    ReportFragment reportFragment=new ReportFragment();
 
                     getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                            new MapFragment()).addToBackStack(null).commit();
-
-
-                }}));
-
-            organisation.setOnClickListener((new View.OnClickListener() {
+                            new ReportFragment()).addToBackStack(null).commit();
+                }
+            });
+            findCard=(CardView)view.findViewById(R.id.infocard);
+            findCard.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
+                    PrelimnaryResultsFragment prelimnaryResultsFragment=new PrelimnaryResultsFragment();
 
                     getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                            new OrganisationviewFragment()).addToBackStack(null).commit();
+                            new PrelimnaryResultsFragment()).addToBackStack(null).commit();
+                }
+            });
+          /*  prepostcard=(CardView)view.findViewById(R.id.prepostcard);
+            prepostcard.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    com.example.meo8.MortalityFragment mortalityFragment=new com.example.meo8.MortalityFragment();
 
+                    getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            new com.example.meo8.MortalityFragment()).addToBackStack(null).commit();
+                }
+            });*/
 
-                }}));
                    return view;
 
         }}
