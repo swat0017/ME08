e0f861c39f74603599b8cd8b4d2e075891a2a725
DarsiniKrishnakumar <70090040+DarsiniKrishnakumar@users.noreply.github.com>
2020-11-02 19:38:03 +1100

    Add files via upload

---
+++ sql/Organisation_table.sql
@@ -0,0 +1,82 @@
+CREATE TABLE Organisations(
+    Name VARCHAR(100),
+    State VARCHAR(5),
+	Address VARCHAR(1500),
+    PhoneNumber INT(15),
+    Website VARCHAR(500),
+    ImageLink VARCHAR(600)
+);
+
+
+
+
+INSERT INTO Organisations(Name, State, Address, PhoneNumber, Website, ImageLink) VALUES ('Ausecology', 'QLD', '3/7 Birubi Street, Coorparoo QLD 4151', '0733974695', 'https://ausecology.com', 'https://me08imagebucket.s3.amazonaws.com/hand-1917895_1920.png');
+
+INSERT INTO Organisations(Name, State, Address, PhoneNumber, Website, ImageLink) VALUES ('Bundaberg Regional Council', 'QLD', 'Bundaberg Administration Centre, 190 Bourbong St, Bundaberg Central QLD 4670', '1300883699', 'https://www.bundaberg.qld.gov.au', 'https://me08imagebucket.s3.amazonaws.com/africa-1603382_1920.jpg');
+
+INSERT INTO Organisations(Name, State, Address, PhoneNumber, Website, ImageLink) VALUES ('Bundaberg Regional Council', 'QLD', 'Bargara Customer Service Centre, Shop 3, 15 See Street, BARGARA QLD 4670', '1300883699', 'https://www.bundaberg.qld.gov.au', 'https://me08imagebucket.s3.amazonaws.com/africa-1603382_1920.jpg');
+
+INSERT INTO Organisations(Name, State, Address, PhoneNumber, Website, ImageLink) VALUES ('Bundaberg Regional Council', 'QLD', 'Childers Service Centre, 45 Churchill Street, CHILDERS QLD 4660', '1300883699', 'https://www.bundaberg.qld.gov.au', 'https://me08imagebucket.s3.amazonaws.com/africa-1603382_1920.jpg');
+
+INSERT INTO Organisations(Name, State, Address, PhoneNumber, Website, ImageLink) VALUES ('Bundaberg Regional Council', 'QLD', 'Gin Gin Service Centre, 4 Dear Street, GIN GIN QLD 4671', '1300883699', 'https://www.bundaberg.qld.gov.au', 'https://me08imagebucket.s3.amazonaws.com/africa-1603382_1920.jpg');
+
+INSERT INTO Organisations(Name, State, Address, PhoneNumber, Website, ImageLink) VALUES ('Bundaberg Regional Council', 'QLD', 'Auswide Building, Level 6, 16 Barolin Street, BUNDABERG QLD 4670', '1300883699', 'https://www.bundaberg.qld.gov.au', 'https://me08imagebucket.s3.amazonaws.com/africa-1603382_1920.jpg');
+
+INSERT INTO Organisations(Name, State, Address, PhoneNumber, Website, ImageLink) VALUES ('Central Queensland University', 'QLD', 'Koala Research CQIRP Building 361 CQUniversity, Ibis Avenue, North Rockhampton, Q 47010', '0741507056', 'https://www.cqu.edu.au/research/organisations/koala-research-cq', 'https://me08imagebucket.s3.amazonaws.com/architecture-5084075_1920.jpg');
+
+INSERT INTO Organisations(Name, State, Address, PhoneNumber, Website, ImageLink) VALUES ('EcoSmart Ecology', 'QLD', '48 Streeton Parade, Everton Park, Qld, 4053', '0731621161', 'https://ecosmartecology.com.au', 'https://me08imagebucket.s3.amazonaws.com/vegan-1343429_1920.png');
+
+INSERT INTO Organisations(Name, State, Address, PhoneNumber, Website, ImageLink) VALUES ('Lake Broadwater Natural History Association', 'QLD', '16–32 Enterprise Street, Bundaberg QLD 4670', '1300130372', 'https://parks.des.qld.gov.au/parks/lake-broadwater/about/culture', 'https://me08imagebucket.s3.amazonaws.com/hands-306713_1280.png');
+
+INSERT INTO Organisations(Name, State, Address, PhoneNumber, Website, ImageLink) VALUES ('Lake Broadwater Natural History Association', 'QLD', 'Level 4, Building 2, William McCormack Place, 5B Sheridan Street, Cairns QLD 4870', '1300130372', 'https://parks.des.qld.gov.au/parks/lake-broadwater/about/culture', 'https://me08imagebucket.s3.amazonaws.com/hands-306713_1280.png');
+
+INSERT INTO Organisations(Name, State, Address, PhoneNumber, Website, ImageLink) VALUES ('Lake Broadwater Natural History Association', 'QLD', 'Hood Street, Charleville QLD 4470', '1300130372', 'https://parks.des.qld.gov.au/parks/lake-broadwater/about/culture', 'https://me08imagebucket.s3.amazonaws.com/hands-306713_1280.png');
+
+INSERT INTO Organisations(Name, State, Address, PhoneNumber, Website, ImageLink) VALUES ('Lake Broadwater Natural History Association', 'QLD', 'Kabool Road, West Burleigh QLD 4219', '1300130372', 'https://parks.des.qld.gov.au/parks/lake-broadwater/about/culture', 'https://me08imagebucket.s3.amazonaws.com/hands-306713_1280.png');
+
+INSERT INTO Organisations(Name, State, Address, PhoneNumber, Website, ImageLink) VALUES ('Lake Broadwater Natural History Association', 'QLD', '34 Cassowary Street, Innisfail QLD 4860', '1300130372', 'https://parks.des.qld.gov.au/parks/lake-broadwater/about/culture', 'https://me08imagebucket.s3.amazonaws.com/hands-306713_1280.png');
+
+INSERT INTO Organisations(Name, State, Address, PhoneNumber, Website, ImageLink) VALUES ('Lake Broadwater Natural History Association', 'QLD', '22–30 Wood Street, Mackay QLD 4740', '1300130372', 'https://parks.des.qld.gov.au/parks/lake-broadwater/about/culture', 'https://me08imagebucket.s3.amazonaws.com/hands-306713_1280.png');
+
+INSERT INTO Organisations(Name, State, Address, PhoneNumber, Website, ImageLink) VALUES ('Lake Broadwater Natural History Association', 'QLD', 'Level 6, Mike Ahern Building, 12 First Avenue, Maroochydore QLD 4558', '1300130372', 'https://parks.des.qld.gov.au/parks/lake-broadwater/about/culture', 'https://me08imagebucket.s3.amazonaws.com/hands-306713_1280.png');
+
+INSERT INTO Organisations(Name, State, Address, PhoneNumber, Website, ImageLink) VALUES ('Lake Broadwater Natural History Association', 'QLD', '102 Lennox Street (Cnr Alice Street), Maryborough QLD 4650', '1300130372', 'https://parks.des.qld.gov.au/parks/lake-broadwater/about/culture', 'https://me08imagebucket.s3.amazonaws.com/hands-306713_1280.png');
+
+INSERT INTO Organisations(Name, State, Address, PhoneNumber, Website, ImageLink) VALUES ('Lake Broadwater Natural History Association', 'QLD', '55 Priors Pocket Road, Moggill', '1300130372', 'https://parks.des.qld.gov.au/parks/lake-broadwater/about/culture', 'https://me08imagebucket.s3.amazonaws.com/hands-306713_1280.png');
+
+INSERT INTO Organisations(Name, State, Address, PhoneNumber, Website, ImageLink) VALUES ('Lake Broadwater Natural History Association', 'QLD', '61 Yeppoon Rd, Parkhurst QLD 4702', '1300130372', 'https://parks.des.qld.gov.au/parks/lake-broadwater/about/culture', 'https://me08imagebucket.s3.amazonaws.com/hands-306713_1280.png');
+
+INSERT INTO Organisations(Name, State, Address, PhoneNumber, Website, ImageLink) VALUES ('Lake Broadwater Natural History Association', 'QLD', '203 Tor Street, Toowoomba QLD 4340', '1300130372', 'https://parks.des.qld.gov.au/parks/lake-broadwater/about/culture', 'https://me08imagebucket.s3.amazonaws.com/hands-306713_1280.png');
+
+INSERT INTO Organisations(Name, State, Address, PhoneNumber, Website, ImageLink) VALUES ('Lake Broadwater Natural History Association', 'QLD', '21 Langton St, Garbutt QLD 4810', '1300130372', 'https://parks.des.qld.gov.au/parks/lake-broadwater/about/culture', 'https://me08imagebucket.s3.amazonaws.com/hands-306713_1280.png');
+
+INSERT INTO Organisations(Name, State, Address, PhoneNumber, Website, ImageLink) VALUES ('Fitzroy River & Coastal Catchments Inc', 'QLD', '14 Wandal Rd, Rockhampton QLD 4700', '0749210573', 'NA', 'https://me08imagebucket.s3.amazonaws.com/vegan-1343429_1920.png');
+
+INSERT INTO Organisations(Name, State, Address, PhoneNumber, Website, ImageLink) VALUES ('Ecosure', 'QLD', '7 Light Street, Fortitude Valley, QLD 4006', '0736061030', 'https://ecosure.com.au', 'https://me08imagebucket.s3.amazonaws.com/volunteer-1888823_1280.png');
+
+INSERT INTO Organisations(Name, State, Address, PhoneNumber, Website, ImageLink) VALUES ('Ecosure', 'QLD', '10/37 Connor Street, Level 4, McDonald House, Burleigh Heads, QLD 4220', '0736061030', 'https://ecosure.com.au', 'https://me08imagebucket.s3.amazonaws.com/volunteer-1888823_1280.png');
+
+INSERT INTO Organisations(Name, State, Address, PhoneNumber, Website, ImageLink) VALUES ('Ecosure', 'QLD', '290 Quay St, Rockhampton, QLD 4700', '0736061030', 'https://ecosure.com.au', 'https://me08imagebucket.s3.amazonaws.com/volunteer-1888823_1280.png');
+
+INSERT INTO Organisations(Name, State, Address, PhoneNumber, Website, ImageLink) VALUES ('Torres Strait Regional Authority', 'QLD', 'Level 1 Torres Strait Haus, 46 Victoria Parade, Thursday Island,Queensland, Australia', '1800079093', 'http://www.tsra.gov.au/', 'https://me08imagebucket.s3.amazonaws.com/animals-2608662_1920.jpg');
+
+INSERT INTO Organisations(Name, State, Address, PhoneNumber, Website, ImageLink) VALUES ('Wildlife Preservation Society of Queensland', 'QLD', 'Suite 1, Level 1,30 Gladstone Road,Highgate Hill, Brisbane, Qld 4101,Australia', '0738440129', 'https://wildlife.org.au/', 'https://me08imagebucket.s3.amazonaws.com/kangaroo-47642_1280.png');
+
+INSERT INTO Organisations(Name, State, Address, PhoneNumber, Website, ImageLink) VALUES ('Mary River Catchment Coordinating Committee', 'QLD', '4570/25 Stewart Terrace, Gympie QLD 4570, Australia', '0754824766', 'https://mrccc.org.au/', 'https://me08imagebucket.s3.amazonaws.com/volunteer-1888823_1280.png');
+
+INSERT INTO Organisations(Name, State, Address, PhoneNumber, Website, ImageLink) VALUES ('Noosa Shire Council', 'QLD', '9 Pelican St, Tewantin QLD 4565, Australia', '0753296500', 'https://www.noosa.qld.gov.au/', 'https://me08imagebucket.s3.amazonaws.com/volunteer-1888823_1280.png');
+
+INSERT INTO Organisations(Name, State, Address, PhoneNumber, Website, ImageLink) VALUES ('Australian Wildlife Conservancy,Sydney', 'NSW', 'L19/1 Farrer Place, Sydney NSW 2000', '0293244210', 'https://www.australianwildlife.org', 'https://me08imagebucket.s3.amazonaws.com/kangaroo-47642_1280.png');
+
+INSERT INTO Organisations(Name, State, Address, PhoneNumber, Website, ImageLink) VALUES ('BirdLife Australia,Sydney', 'NSW', 'Newington Armory, Building 133, 1 Jamieson Street, Sydney Olympic Park NSW 2127', '0296471033', 'https://birdlife.org.au', 'https://me08imagebucket.s3.amazonaws.com/curlew-33036_1280.png');
+
+INSERT INTO Organisations(Name, State, Address, PhoneNumber, Website, ImageLink) VALUES ('Ecosure', 'SA', '36a/11-13, Park Way, Mawson Lakes, SA 5095', '0882602580', 'https://ecosure.com.au', 'https://me08imagebucket.s3.amazonaws.com/permaculture-3209841_1280.png');
+
+INSERT INTO Organisations(Name, State, Address, PhoneNumber, Website, ImageLink) VALUES ('Australian Wildlife Conservancy,Melbourne', 'VIC', '65 Flinders Lane Melbourne, VIC ,3000', '0386606614', 'https: //www.australianwildlife.org', 'https://me08imagebucket.s3.amazonaws.com/paw-print-35684_1280.png');
+
+INSERT INTO Organisations(Name, State, Address, PhoneNumber, Website, ImageLink) VALUES ('BirdLife Australia,Melbourne', 'VIC', 'Suite 2 - 05,60 Leicester Street,Carlton VIC 3053', '0393470757', 'https: //birdlife.org.au', 'https://me08imagebucket.s3.amazonaws.com/curlew-33036_1280.png');
+
+INSERT INTO Organisations(Name, State, Address, PhoneNumber, Website, ImageLink) VALUES ('Australian Wildlife Conservancy,Perth', 'WA', 'Suite 5, 280 Hay St, Subiaco Western Australia 6008', '0893809633', 'https://www.australianwildlife.org', 'https://me08imagebucket.s3.amazonaws.com/paw-print-35684_1280.png');
+
+INSERT INTO Organisations(Name, State, Address, PhoneNumber, Website, ImageLink) VALUES ('BirdLife Australia,Perth', 'WA', 'Peregrine House, 167 Perry Lakes Drive, Floreat WA 6014', '0893837749', 'https://birdlife.org.au', 'https://me08imagebucket.s3.amazonaws.com/curlew-33036_1280.png');
+
